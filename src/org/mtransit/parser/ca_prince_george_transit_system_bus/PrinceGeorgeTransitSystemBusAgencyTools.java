c064d96adf3ea2a5adf7c757aad56c88b0d5cbea
Mathieu Méa <mathieu.mea@gmail.com>
2015-07-13 17:25:18 -0400

    Minor improvements...

--- src/org/mtransit/parser/ca_prince_george_transit_system_bus/PrinceGeorgeTransitSystemBusAgencyTools.java
+++ src/org/mtransit/parser/ca_prince_george_transit_system_bus/PrinceGeorgeTransitSystemBusAgencyTools.java
@@ -35,7 +35,7 @@ public class PrinceGeorgeTransitSystemBusAgencyTools extends DefaultAgencyTools 
 
 	@Override
 	public void start(String[] args) {
-		System.out.printf("\nGenerating Prince George Transit System bus data...\n");
+		System.out.printf("\nGenerating Prince George Transit System bus data...");
 		long start = System.currentTimeMillis();
 		this.serviceIds = extractUsefulServiceIds(args, this);
 		super.start(args);
